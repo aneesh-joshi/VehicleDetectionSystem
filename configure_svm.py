940cda04c8740ef4478aa745ea6925b4a67d473e
Aneesh Joshi <Aneesh.Joshi@citrix.com>
2018-01-10 20:05:06 +0530

    make classifier

---
+++ configure_svm.py
@@ -0,0 +1,60 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+import cv2
+import sys
+from pprint import pprint
+
+import time
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from detection_lib import color_hist, bin_spatial, get_hog_features, extract_color_features, extract_hog_features
+
+vehicle_images = glob.glob('vehicle_data/vehicles/*/*.png', recursive=True)
+non_vehicle_images = glob.glob('vehicle_data/non-vehicles/*/*.png', recursive=True)
+# print(vehicle_images)
+print('FOUND :', len(vehicle_images), 'Vehicle Images')
+print('FOUND :', len(non_vehicle_images), 'Non Vehicle Images')
+
+
+spatial = 32
+histbin = 5
+
+# vehicle_features = extract_color_features(vehicle_images, spatial_size=(spatial, spatial), hist_bins=histbin)
+# non_vehicle_features = extract_color_features(non_vehicle_images, spatial_size=(spatial, spatial), hist_bins=histbin)
+
+vehicle_features = extract_hog_features(vehicle_images)
+non_vehicle_features = extract_hog_features(non_vehicle_images)
+
+X = np.vstack((vehicle_features, non_vehicle_features)).astype(np.float64)
+X_scaler = StandardScaler().fit(X)
+scaled_X = X_scaler.transform(X)
+
+y = np.hstack( (np.ones(len(vehicle_features)), np.zeros(len(non_vehicle_features))) )
+
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.5, random_state=rand_state)
+
+print('Using spatial binning of:',spatial, 'and', histbin,'histogram bins')
+print('Feature vector length:', len(X_train[0]))
+
+# Use a linear SVC
+svc = LinearSVC()
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+# Check the prediction time for a single sample
+t=time.time()
+n_predict = 10
+print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+t2 = time.time()
+print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
---
+++ detection_lib.py
@@ -0,0 +1,189 @@
+'''
+A library defining all the usefull functions
+'''
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from skimage.feature import hog
+import sys
+
+# Define a function to compute color histogram features
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    # Convert image to new color space (if specified)
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    else: feature_image = np.copy(img)
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(feature_image, size).ravel()
+    # Return the feature vector
+    return features
+
+# Define a function to compute binned color features
+def bin_spatial(img, size=(32, 32)):
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(img, size).ravel()
+    # Return the feature vector
+    return features
+
+# Define a function to compute color histogram features
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
+
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
+    if vis == True:
+        # Use skimage.hog() to get both features and a visualization
+        features, hog_image = hog(img, orientations=orient,
+                          pixels_per_cell=(pix_per_cell, pix_per_cell),
+                          cells_per_block=(cell_per_block, cell_per_block),
+                          visualise=True, feature_vector=False)
+        return features, hog_image
+    else:
+        # Use skimage.hog() to get features only
+        features = hog(img, orientations=orient,
+                          pixels_per_cell=(pix_per_cell, pix_per_cell),
+                          cells_per_block=(cell_per_block, cell_per_block),
+                          visualise=False, feature_vector=feature_vec)
+        return features
+
+# Define a function to extract features from a list of images
+# Have this function call bin_spatial() and color_hist()
+def extract_color_features(imgs, cspace='RGB', spatial_size=(32, 32),
+                        hist_bins=32, hist_range=(0, 256)):
+    # Create a list to append feature vectors to
+    features = []
+
+    total_files = str(len(imgs))
+    # Iterate through the list of images
+    for i, file in enumerate(imgs):
+        # Read in each one by one
+        image = mpimg.imread(file)
+        # apply color conversion if other than 'RGB'
+        if cspace != 'RGB':
+            if cspace == 'HSV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            elif cspace == 'LUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            elif cspace == 'HLS':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            elif cspace == 'YUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        else: feature_image = np.copy(image)
+        # Apply bin_spatial() to get spatial color features
+        spatial_features = bin_spatial(feature_image, size=spatial_size)
+        # Apply color_hist() also with a color space option now
+        hist_features = color_hist(feature_image, nbins=hist_bins, bins_range=hist_range)
+        # Append the new feature vector to the features list
+        features.append(np.hstack((spatial_features, hist_features)))
+
+        sys.stdout.write("Progress:" + str(i+1) + " / " + total_files + "\r")
+        sys.stdout.flush()
+    # Return list of feature vectors
+    print('\nDONE')
+    return features
+
+# Define a function to extract features from a list of images
+# Have this function call bin_spatial() and color_hist()
+def extract_hog_features(imgs, cspace='RGB', orient=9,
+                        pix_per_cell=8, cell_per_block=2, hog_channel=0):
+    # Create a list to append feature vectors to
+    features = []
+    total_files = str(len(imgs))
+    # Iterate through the list of images
+    for i, file in enumerate(imgs):
+        # Read in each one by one
+        image = mpimg.imread(file)
+        # apply color conversion if other than 'RGB'
+        if cspace != 'RGB':
+            if cspace == 'HSV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            elif cspace == 'LUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            elif cspace == 'HLS':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            elif cspace == 'YUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+            elif cspace == 'YCrCb':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+        else: feature_image = np.copy(image)
+
+        # Call get_hog_features() with vis=False, feature_vec=True
+        if hog_channel == 'ALL':
+            hog_features = []
+            for channel in range(feature_image.shape[2]):
+                hog_features.append(get_hog_features(feature_image[:,:,channel],
+                                    orient, pix_per_cell, cell_per_block,
+                                    vis=False, feature_vec=True))
+            hog_features = np.ravel(hog_features)
+        else:
+            hog_features = get_hog_features(feature_image[:,:,hog_channel], orient,
+                        pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+        # Append the new feature vector to the features list
+        features.append(hog_features)
+        sys.stdout.write("Progress:" + str(i+1) + " / " + total_files + "\r")
+        sys.stdout.flush()
+
+    print('\nDONE')
+
+    # Return list of feature vectors
+    return features
+
+# draws the given boxes on the given image
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
+
+# Define a function to search for template matches
+# and return a list of bounding boxes
+def find_matches(img, template_list):
+    # Define an empty list to take bbox coords
+    bbox_list = []
+    # Define matching method
+    # Other options include: cv2.TM_CCORR_NORMED', 'cv2.TM_CCOEFF', 'cv2.TM_CCORR',
+    #         'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED'
+    method = cv2.TM_CCOEFF_NORMED
+    # Iterate through template list
+    for temp in template_list:
+        # Read in templates one by one
+        tmp = mpimg.imread(temp)
+        # Use cv2.matchTemplate() to search the image
+        result = cv2.matchTemplate(img, tmp, method)
+        # Use cv2.minMaxLoc() to extract the location of the best match
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        # Determine a bounding box for the match
+        w, h = (tmp.shape[1], tmp.shape[0])
+        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+            top_left = min_loc
+        else:
+            top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        # Append bbox position to list
+        bbox_list.append((top_left, bottom_right))
+        # Return the list of bounding boxes
+
+    return bbox_list
