651ef5004da8a6ced84fe342350104320789c8cf
Aneesh Joshi <Aneesh.Joshi@citrix.com>
2018-01-12 12:03:24 +0530

    try some stuff

--- configure_svm.py
+++ configure_svm.py
@@ -5,6 +5,7 @@ import glob
 import cv2
 import sys
 from pprint import pprint
+import pickle
 
 import time
 from sklearn.model_selection import train_test_split
@@ -19,42 +20,54 @@ print('FOUND :', len(vehicle_images), 'Vehicle Images')
 print('FOUND :', len(non_vehicle_images), 'Non Vehicle Images')
 
 
+def process_image(image, params):
+
+
+
 spatial = 32
 histbin = 5
 
-# vehicle_features = extract_color_features(vehicle_images, spatial_size=(spatial, spatial), hist_bins=histbin)
-# non_vehicle_features = extract_color_features(non_vehicle_images, spatial_size=(spatial, spatial), hist_bins=histbin)
+params['spatial'] = spatial
+params['histbin'] = histbin
 
-vehicle_features = extract_hog_features(vehicle_images)
-non_vehicle_features = extract_hog_features(non_vehicle_images)
+vehicle_color_features = extract_color_features(vehicle_images, spatial_size=(spatial, spatial), hist_bins=histbin)
+non_vehicle_color_features = extract_color_features(non_vehicle_images, spatial_size=(spatial, spatial), hist_bins=histbin)
+
+vehicle_features = np.hstack((extract_hog_features(vehicle_images), vehicle_color_features))
+non_vehicle_features = np.hstack((extract_hog_features(non_vehicle_images), non_vehicle_color_features))
 
 X = np.vstack((vehicle_features, non_vehicle_features)).astype(np.float64)
 X_scaler = StandardScaler().fit(X)
 scaled_X = X_scaler.transform(X)
 
-y = np.hstack( (np.ones(len(vehicle_features)), np.zeros(len(non_vehicle_features))) )
+# y = np.hstack( (np.ones(len(vehicle_features)), np.zeros(len(non_vehicle_features))) )
 
 
-# Split up data into randomized training and test sets
-rand_state = np.random.randint(0, 100)
-X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.5, random_state=rand_state)
+# # Split up data into randomized training and test sets
+# rand_state = np.random.randint(0, 100)
+# X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2, random_state=rand_state)
 
-print('Using spatial binning of:',spatial, 'and', histbin,'histogram bins')
-print('Feature vector length:', len(X_train[0]))
+# print('Using spatial binning of:',spatial, 'and', histbin,'histogram bins')
+# print('Feature vector length:', len(X_train[0]))
 
-# Use a linear SVC
-svc = LinearSVC()
-# Check the training time for the SVC
-t=time.time()
-svc.fit(X_train, y_train)
-t2 = time.time()
-print(round(t2-t, 2), 'Seconds to train SVC...')
-# Check the score of the SVC
-print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
-# Check the prediction time for a single sample
-t=time.time()
-n_predict = 10
-print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
-print('For these',n_predict, 'labels: ', y_test[0:n_predict])
-t2 = time.time()
-print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+# # Use a linear SVC
+# svc = LinearSVC()
+# # Check the training time for the SVC
+# t=time.time()
+# svc.fit(X_train, y_train)
+# t2 = time.time()
+# print(round(t2-t, 2), 'Seconds to train SVC...')
+# # Check the score of the SVC
+# print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+# # Check the prediction time for a single sample
+# t=time.time()
+# n_predict = 10
+# print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+# print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+# t2 = time.time()
+# print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+
+# print('SAVING MODEL')
+# with open('models/temp_model.pkl', 'wb') as f:
+#     pickle.dump([svc, X_scaler], f)
+# print('MODEL SAVED')
---
+++ detect_cars.py
@@ -0,0 +1,149 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pickle
+
+from detection_lib import *
+
+image = mpimg.imread('cutout1.jpg')
+
+# Define a function that takes an image,
+# start and stop positions in both x and y,
+# window size (x and y dimensions),
+# and overlap fraction (for both x and y)
+# Define a function that takes an image,
+# start and stop positions in both x and y,
+# window size (x and y dimensions),
+# and overlap fraction (for both x and y)
+def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
+                    xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
+    # If x and/or y start/stop positions not defined, set to image size
+    if x_start_stop[0] == None:
+        x_start_stop[0] = 0
+    if x_start_stop[1] == None:
+        x_start_stop[1] = img.shape[1]
+    if y_start_stop[0] == None:
+        y_start_stop[0] = 0
+    if y_start_stop[1] == None:
+        y_start_stop[1] = img.shape[0]
+
+    # Compute the span of the region to be searched
+    xspan = x_start_stop[1] - x_start_stop[0]
+    yspan = y_start_stop[1] - y_start_stop[0]
+    # Compute the number of pixels per step in x/y
+    nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
+    # Compute the number of windows in x/y
+    nx_buffer = np.int(xy_window[0]*(xy_overlap[0]))
+    ny_buffer = np.int(xy_window[1]*(xy_overlap[1]))
+    nx_windows = np.int((xspan-nx_buffer)/nx_pix_per_step)
+    ny_windows = np.int((yspan-ny_buffer)/ny_pix_per_step)
+    # Initialize a list to append window positions to
+    window_list = []
+    # Loop through finding x and y window positions
+    # Note: you could vectorize this step, but in practice
+    # you'll be considering windows one by one with your
+    # classifier, so looping makes sense
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            # Calculate window position
+            startx = xs*nx_pix_per_step + x_start_stop[0]
+            endx = startx + xy_window[0]
+            starty = ys*ny_pix_per_step + y_start_stop[0]
+            endy = starty + xy_window[1]
+            # Append window position to list
+            window_list.append(((startx, starty), (endx, endy)))
+    # Return the list of windows
+    return window_list
+
+# Define a function to extract features from a single image window
+# This function is very similar to extract_features()
+# just for a single image rather than list of images
+def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
+                        hist_bins=32, orient=9,
+                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
+                        spatial_feat=True, hist_feat=True, hog_feat=True):
+
+    vehicle_color_features = extract_color_features(['test_image.jpg'], spatial_size=(32, 32), hist_bins=5)
+
+    vehicle_features = np.hstack((extract_hog_features(['test_image.jpg']), vehicle_color_features))
+
+    return vehicle_features
+
+# Define a function you will pass an image
+# and the list of windows to be searched (output of slide_windows())
+def search_windows(img, windows, clf, scaler, color_space='RGB',
+                    spatial_size=(32, 32), hist_bins=32,
+                    hist_range=(0, 256), orient=9,
+                    pix_per_cell=8, cell_per_block=2,
+                    hog_channel=0, spatial_feat=True,
+                    hist_feat=True, hog_feat=True):
+
+    #1) Create an empty list to receive positive detection windows
+    on_windows = []
+    #2) Iterate over all windows in the list
+    for window in windows:
+        #3) Extract the test window from original image
+        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))
+        #4) Extract features for that window using single_img_features()
+        features = single_img_features(test_img, color_space=color_space,
+                            spatial_size=spatial_size, hist_bins=hist_bins,
+                            orient=orient, pix_per_cell=pix_per_cell,
+                            cell_per_block=cell_per_block,
+                            hog_channel=hog_channel, spatial_feat=spatial_feat,
+                            hist_feat=hist_feat, hog_feat=hog_feat)
+        #5) Scale extracted features to be fed to classifier
+        print(features)
+        print(features.shape)
+        reshaped_features = np.array(features).reshape(1, -1)
+        test_features = scaler.transform(reshaped_features)
+        #6) Predict using your classifier
+        prediction = clf.predict(test_features)
+        #7) If positive (prediction == 1) then save the window
+        if prediction == 1:
+            on_windows.append(window)
+    #8) Return windows for positive detections
+    return on_windows
+
+
+with open('models/temp_model.pkl', 'rb') as f:
+    svc, X_scaler = pickle.load(f)
+print('PICKLE LOADED')
+
+image = mpimg.imread('test_image.jpg')
+draw_image = np.copy(image)
+
+### TODO: Tweak these parameters and see how the results change.
+color_space = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 9  # HOG orientations
+pix_per_cell = 8 # HOG pixels per cell
+cell_per_block = 2 # HOG cells per block
+hog_channel = 0 # Can be 0, 1, 2, or "ALL"
+spatial_size = (16, 16) # Spatial binning dimensions
+hist_bins = 5    # Number of histogram bins
+
+spatial_feat = True # Spatial features on or off
+hist_feat = True # Histogram features on or off
+hog_feat = True # HOG features on or off
+y_start_stop = [image.shape[0]//2, image.shape[0]] # Min and max in y to search in slide_window()
+print(y_start_stop)
+
+windows = slide_window(image, x_start_stop=[None, None], y_start_stop=y_start_stop,
+                    xy_window=(96, 96), xy_overlap=(0.5, 0.5))
+
+hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_space,
+                        spatial_size=spatial_size, hist_bins=hist_bins,
+                        orient=orient, pix_per_cell=pix_per_cell,
+                        cell_per_block=cell_per_block,
+                        hog_channel=hog_channel, spatial_feat=spatial_feat,
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+
+window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
+
+
+
+
+plt.imshow(window_img)
+plt.imshow(window_img)
+plt.show()
