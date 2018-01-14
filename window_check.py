8178dbd176eb1da76ae6607986c5e5419ce5129a
Aneesh Joshi <Aneesh.Joshi@citrix.com>
2018-01-14 18:54:48 +0530

    make a working bounding box system

--- window_check.py
+++ window_check.py
@@ -1,6 +1,39 @@
 import glob
 import pickle
 from new_svm_configure import *
+from scipy.ndimage.measurements import label
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap# Iterate through list of bboxes
+    
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image
+    return img
+
 
 with open('model.pkl', 'rb') as f:
 	svc, X_scaler = pickle.load(f)
@@ -27,7 +60,7 @@ hist_feat = True # Histogram features on or off
 hog_feat = True # HOG features on or off
 y_start_stop = [360, 720] # Min and max in y to search in slide_window()
 
-xy_overlap=(0.5,0.8)
+xy_overlap=(0.5,0.9)
 xy_window =(96, 96)
 
 for image_path in glob.glob('test_images/*'):
@@ -47,5 +80,60 @@ for image_path in glob.glob('test_images/*'):
 
 	window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)                    
 
-	plt.imshow(window_img)
+	# plt.imshow(window_img)
+	# plt.show()
+
+	heat = np.zeros_like(image[:,:,0]).astype(np.float)
+	box_list = hot_windows
+
+	# Add heat to each box in box list
+	heat = add_heat(heat,box_list)
+	    
+	# Apply threshold to help remove false positives
+	heat = apply_threshold(heat, 2)
+
+	# Visualize the heatmap when displaying    
+	heatmap = np.clip(heat, 0, 255)
+
+	# Find final boxes from heatmap using label function
+	labels = label(heatmap)
+	draw_img = draw_labeled_bboxes(np.copy(image), labels)
+
+	plt.imshow(draw_img)
 	plt.show()
+
+	plt.imshow(heatmap)
+	plt.show()
+	
+
+lel
+# Read in a pickle file with bboxes saved
+# Each item in the "all_bboxes" list will contain a 
+# list of boxes for one of the images shown above
+box_list = pickle.load( open( "bbox_pickle.p", "rb" ))
+
+# Read in image similar to one shown above 
+image = mpimg.imread('test_image.jpg')
+heat = np.zeros_like(image[:,:,0]).astype(np.float)
+
+# Add heat to each box in box list
+heat = add_heat(heat,box_list)
+    
+# Apply threshold to help remove false positives
+heat = apply_threshold(heat,1)
+
+# Visualize the heatmap when displaying    
+heatmap = np.clip(heat, 0, 255)
+
+# Find final boxes from heatmap using label function
+labels = label(heatmap)
+draw_img = draw_labeled_bboxes(np.copy(image), labels)
+
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(draw_img)
+plt.title('Car Positions')
+plt.subplot(122)
+plt.imshow(heatmap, cmap='hot')
+plt.title('Heat Map')
+fig.tight_layout()
